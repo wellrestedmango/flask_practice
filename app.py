cfb3af42aecd393e770d5ecf4a699349bab9f00f
ericfassbender@proton.me <ericfassbender@proton.me>
2024-08-27 20:33:11 -0500

    fixing app

--- app.py
+++ app.py
@@ -13,7 +13,11 @@ class Todo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     content = db.Column(db.String(200), nullable=False)
     date_created = db.Column(db.DateTime, default=datetime.utcnow)
-
+    
+    def __repr__(self):
+        return '<Task %r>' % self.id
+    
+    
 @app.route("/")
 def hello_world():
     return render_template('index.html')
