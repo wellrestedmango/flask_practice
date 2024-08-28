6e2fbe3c52eb4b9efc87eb41d7bfffd4f8060725
ericfassbender@proton.me <ericfassbender@proton.me>
2024-08-27 20:27:50 -0500

    starting out

--- app.py
+++ app.py
@@ -2,8 +2,17 @@ import flask
 from flask import Flask
 from flask import render_template
 from flask import url_for
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+db = SQLAlchemy(app)
+
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(200), nullable=False)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
 
 @app.route("/")
 def hello_world():
